e62079b8bd0114efa11f1e1f0fbd70485d84669a
JeremyM45 <jeremyvs1945@gmail.com>
2022-05-05 12:00:07 -0700

    add inital files

---
+++ NumbersToWords.Test/ModelTests/NumToWordsTest.cs
@@ -0,0 +1,9 @@
+using NumToWords;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+namespace NumToWords.Test
+{
+  public class NumConverterTests
+  {
+    
+  }
+}
---
+++ NumbersToWords/Models/NumToWords.cs
@@ -0,0 +1,7 @@
+namespace NumToWords
+{
+  class NumConverter
+  {
+    
+  }
+}
